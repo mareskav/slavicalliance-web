b7852f0671854cec57859e751d1d76d4d07fc625
mareskav <mareska.vaclav@centrum.cz>
2026-05-28 20:21:11 +0200

    Changes for KvizyPage

--- apps/site/src/app/kvizy/page.tsx
+++ apps/site/src/app/kvizy/page.tsx
@@ -9,12 +9,11 @@ export const metadata: Metadata = {
 
 const KvizyPage = () => {
   return (
-    <div className="space-y-10 font-sans">
+    <div className="space-y-5 font-sans">
       <section>
-        <div className="text-sm font-bold uppercase text-sky-100/60">Hospodský kvíz</div>
-        <h1 className="mt-3 text-4xl font-bold text-white sm:text-5xl">Kvízy</h1>
-        <p className="mt-4 max-w-3xl text-base font-medium leading-7 text-white/62 sm:text-lg">
-          Najdi, kdy jdou jednotlivé týmy na Hospodský kvíz.
+        <h1 className="mt-6 text-4xl font-bold tracking-tight text-white sm:text-4xl">Kvízy</h1>
+        <p className="mt-3 max-w-3xl text-base font-medium leading-6 text-white/58 sm:text-lg">
+          Najdi, kdy jdou jednotlivé týmy na Hospodský kvíz
         </p>
       </section>
 
